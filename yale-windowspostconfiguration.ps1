8216dcf0a78476a232bae2b9521cbaf1b982add5
Ken Hoover <ken.hoover@yale.edu>
2017-06-15 13:32:38 -0400

    fixed server parameter name

--- yale-windowspostconfiguration.ps1
+++ yale-windowspostconfiguration.ps1
@@ -1,5 +1,5 @@
 # yale-windowspostconfiguration.ps1
 
-send-mailmessage -To "ken.hoover@yale.edu" -From "azurebuilds@yale.edu" -server mail.yale.edu -Subject "Build succeeded for $env:computername" -Body "Build of $env:computername complete"
+send-mailmessage -To "ken.hoover@yale.edu" -From "azurebuilds@yale.edu" -smtpserver mail.yale.edu -Subject "Build succeeded for $env:computername" -Body "Build of $env:computername complete"
 
 
