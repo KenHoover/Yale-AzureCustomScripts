a244c2a76740821b8960460ea5daa2e48e52fcf9
Ken Hoover <ken.hoover@yale.edu>
2017-06-16 10:10:27 -0400

    Update yale-windowspostconfiguration.ps1

--- yale-windowspostconfiguration.ps1
+++ yale-windowspostconfiguration.ps1
@@ -1,5 +1,9 @@
 # yale-windowspostconfiguration.ps1
 
+# Install Telnet Client
+Install-WindowsFeature telnet-client
+
+# Send email to indicate that we're done.
 send-mailmessage -To "ken.hoover@yale.edu" -From "azurebuilds@yale.edu" -smtpserver mail.yale.edu -Subject "Build succeeded for $env:computername" -Body "Build of $env:computername complete"
 
 
