0f7526bac0432657a7b9b6090ff552f488433639
Ken Hoover <ken.hoover@yale.edu>
2017-07-10 12:05:18 -0400

    disable telnet install

--- yale-windowspostconfiguration.ps1
+++ yale-windowspostconfiguration.ps1
@@ -1,7 +1,7 @@
 # yale-windowspostconfiguration.ps1
 
 # Install Telnet Client
-Install-WindowsFeature telnet-client
+# Install-WindowsFeature telnet-client
 
 # Send email to indicate that we're done.
 send-mailmessage -To "ken.hoover@yale.edu" -From "azurebuilds@yale.edu" -smtpserver mail.yale.edu -Subject "Build succeeded for $env:computername" -Body "Build of $env:computername complete"
