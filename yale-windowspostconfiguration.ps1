1c5f702a545a6c05c19329a7084c102752e5f44c
Ken Hoover <ken.hoover@yale.edu>
2017-07-10 13:39:54 -0400

    add user to local admins

--- yale-windowspostconfiguration.ps1
+++ yale-windowspostconfiguration.ps1
@@ -1,7 +1,12 @@
+
 # yale-windowspostconfiguration.ps1
 
-# Install Telnet Client
-# Install-WindowsFeature telnet-client
+Param(
+    [Parameter (mandatory=$true)][String]$VMOwnerUsername
+)
+
+# Add VM owner to local Administrators group
+net localgroup Administrators /add yale\$VMOwnerUsername
 
 # Send email to indicate that we're done.
 send-mailmessage -To "ken.hoover@yale.edu" -From "azurebuilds@yale.edu" -smtpserver mail.yale.edu -Subject "Build succeeded for $env:computername" -Body "Build of $env:computername complete"
