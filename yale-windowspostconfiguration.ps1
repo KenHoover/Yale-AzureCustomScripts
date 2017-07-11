8c458ff5d0203b3ec55402b8ff4fb659c084b5e9
Ken Hoover <ken.hoover@yale.edu>
2017-07-11 10:53:45 -0400

    more username alignment

--- yale-windowspostconfiguration.ps1
+++ yale-windowspostconfiguration.ps1
@@ -1,14 +1,14 @@
 # yale-windowspostconfiguration.ps1
 
 Param(
-    [Parameter (mandatory=$true)][String]$VMOwnerUsername
+    [Parameter (mandatory=$true)][String]$vmOwnerNetid
 )
 
 # Add VM owner to local Administrators group
-$member = "yale\$VMOwnerUsername"
+$member = "yale\$vmOwnerNetid"
 Add-LocalGroupMember -Group Administrators -Member $member
 
 # Send email to indicate that we're done.
-send-mailmessage -To "ken.hoover@yale.edu" -From "azurebuilds@yale.edu" -smtpserver mail.yale.edu -Subject "Build succeeded for $env:computername" -Body "Build of $env:computername for $VMOwnerUsername complete"
+send-mailmessage -To "ken.hoover@yale.edu" -From "azurebuilds@yale.edu" -smtpserver mail.yale.edu -Subject "Build succeeded for $env:computername" -Body "Build of $env:computername for $member complete"
 
 
